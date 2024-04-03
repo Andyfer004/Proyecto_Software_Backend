7f66c3ff33ebd9e27d63e1edaf6795e50dca2435
Diederich Solis <105893797+DiederichSolis@users.noreply.github.com>
2024-04-02 18:44:30 -0600

    feat: modify file usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    // Constructor
+    public function __construct()
+    {
+        
+    }
+
+    // Método index
+    public function index()
+    {
+       
+    }
 }
