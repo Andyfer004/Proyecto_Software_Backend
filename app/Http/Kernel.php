3a64ab60a5707148d69adfce381576883ab7093a
Diederich Solis <105893797+DiederichSolis@users.noreply.github.com>
2024-04-02 23:15:11 -0600

    feat: edit error

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,8 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
-        //middleware para la autenticacion
-        'auth' => \App\Http\Middleware\AuthMiddleware::class
-    
+        'logged' => \App\Http\Middleware\loggedMiddleware::class,
+
     ];
 }
