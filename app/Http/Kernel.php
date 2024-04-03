404ea252802b0e35194850ca99afc62e808d57ef
Diederich Solis <105893797+DiederichSolis@users.noreply.github.com>
2024-04-02 22:45:54 -0600

    feat: create middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        //middleware para la autenticacion
+        'auth' => \App\Http\Middleware\AuthMiddleware::class
+    
     ];
 }
