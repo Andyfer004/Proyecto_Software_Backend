c2cec4b76f61d12ef64fa77b6cd2937b05a817e0
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-08 23:42:54 -0600

    feat: create model status

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -5,26 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Subtask extends Model
+class Status extends Model
 {
     use HasFactory;
 
 
-    protected $table ="subtasks";
+    protected $table ="status";
 
     protected $primaryKey="id";
 
-
     protected $fillable = [
         'id',
-        'name',
-        'description',
-        'priorityid',
-        'duedate',
-        'timeestimatehours',
-        'taskid',
-        'statusid',
+        'statusname',
         'created_at',
         'updated_at',
     ];
+
+
 }
