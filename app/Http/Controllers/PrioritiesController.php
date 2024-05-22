67ee7ce36bd832e0532b1f81730efa624f0085d3
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-21 19:11:39 -0600

    feat: add funcion de update

--- app/Http/Controllers/PrioritiesController.php
+++ app/Http/Controllers/PrioritiesController.php
@@ -20,4 +20,22 @@ class PrioritiesController extends Controller
         return response()->json(['message' => 'Prioridad creada correctamente', 'priority' => $priority], 201);
     }
 
+    public function updatePriority(Request $request, $id)
+    {
+        $request->validate([
+            'namepriority' => 'required|string|max:50',
+        ]);
+
+        $priority = Priorities::find($id);
+        if (!$priority) {
+            return response()->json(['message' => 'Prioridad no encontrada'], 404);
+        }
+
+        $priority->namepriority = $request->namepriority;
+        $priority->save();
+
+        return response()->json(['message' => 'Prioridad actualizada correctamente', 'priority' => $priority], 200);
+    }
+    
+
 }
