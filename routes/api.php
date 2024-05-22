4ad5ee3cb9a60def016f6a0b31445c87ae90cbaa
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-21 18:54:52 -0600

    feat: add route

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\StatusController;
 Route::post('status', [StatusController::class, 'addStatus']);
 Route::put('status/{id}', [StatusController::class, 'updateStatus']);
 Route::delete('status/{id}', [StatusController::class, 'deleteStatus']);
+Route::get('/status/{id}', [StatusController::class, 'getStatus']);
+Route::get('/statuses', [StatusController::class, 'getStatuses']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
