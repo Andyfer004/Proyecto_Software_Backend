49cc55342f6c72183e6a191ce51f11fb9f092ea3
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-21 21:07:23 -0600

    feat: funcion update

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -25,4 +25,26 @@ class NotesController extends Controller
         return response()->json(['message' => 'Nota creada correctamente', 'note' => $note], 201);
     }
 
+    public function updateNote(Request $request, $id)
+    {
+        $request->validate([
+            'note' => 'required|string|max:255',
+            'image' => 'nullable|string|max:255',
+            'profile_id' => 'required|integer',
+        ]);
+
+        $note = Notes::find($id);
+        if (!$note) {
+            return response()->json(['message' => 'Nota no encontrada'], 404);
+        }
+
+        $note->note = $request->note;
+        $note->image = $request->image;
+        $note->profile_id = $request->profile_id;
+        $note->save();
+
+        return response()->json(['message' => 'Nota actualizada correctamente', 'note' => $note], 200);
+    }
+
+
 }
