2ccee53d74bb7d3472546f3c615aa4df2e78c984
Jorge162017 <lop221038@uvg.edu.gt>
2024-05-21 23:50:03 -0600

    rutas del controlador events

--- routes/userRoutes.php
+++ routes/userRoutes.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 
     Route::post('login', 'UserController@login');
     Route::post('register', 'UserController@register');
+
+    Route::get('events', [EventController::class, 'index']);
+    Route::get('events/{id}', [EventController::class, 'show']);
+    Route::post('events', [EventController::class, 'store']);
+    Route::put('events/{id}', [EventController::class, 'update']);
+    Route::delete('events/{id}', [EventController::class, 'destroy']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -16,3 +23,5 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::get('/not_auth', function () {
     return response()->json(['message' => 'not authenticated'], 200);
 });
+
+
