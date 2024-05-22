f75267769d0714f90b027229aeb45ece475b86bb
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2024-05-21 23:51:30 -0600

    se arreglaron las rutas

--- routes/userRoutes.php
+++ routes/userRoutes.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 
     Route::post('login', 'UserController@login');
     Route::post('register', 'UserController@register');
-    Route::resource('profiles', 'ProfileController');
+
+    //rutas del controlador ProfileController
+    Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
+    Route::get('/profiles/create', [ProfileController::class, 'create'])->name('profiles.create');
+    Route::post('/profiles', [ProfileController::class, 'store'])->name('profiles.store');
+    Route::get('/profiles/{profile}', [ProfileController::class, 'show'])->name('profiles.show');
+    Route::get('/profiles/{profile}/edit', [ProfileController::class, 'edit'])->name('profiles.edit');
+    Route::put('/profiles/{profile}', [ProfileController::class, 'update'])->name('profiles.update');
+    Route::delete('/profiles/{profile}', [ProfileController::class, 'destroy'])->name('profiles.destroy');
 
 
 Route::middleware('auth:sanctum')->group(function () {
