ad719e4b292410e4afb3536e6ab3356e04630d98
Jorge162017 <lop221038@uvg.edu.gt>
2024-05-21 23:47:53 -0600

    rutas del controlador task

--- routes/userRoutes.php
+++ routes/userRoutes.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 
     Route::post('login', 'UserController@login');
     Route::post('register', 'UserController@register');
+
+    Route::get('tasks', [TaskController::class, 'index']);
+    Route::get('tasks/{id}', [TaskController::class, 'show']);
+    Route::post('tasks', [TaskController::class, 'store']);
+    Route::put('tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -16,3 +23,5 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::get('/not_auth', function () {
     return response()->json(['message' => 'not authenticated'], 200);
 });
+
+
