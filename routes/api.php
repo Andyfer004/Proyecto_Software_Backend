80f84005ece873e22d2cc2ea2515c7b92adecce2
Jorge162017 <lop221038@uvg.edu.gt>
2024-05-24 10:39:49 -0600

    rutas taskController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StatusController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,18 @@ use App\Http\Controllers\StatusController;
 Route::post('status', [StatusController::class, 'addStatus']);
 Route::put('status/{id}', [StatusController::class, 'updateStatus']);
 Route::delete('status/{id}', [StatusController::class, 'deleteStatus']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Rutas del controlador taskController
+
+Route::post('tasks', [TaskController::class, 'addTask']);
+Route::put('tasks/{id}', [TaskController::class, 'updateTask']);
+Route::delete('tasks/{id}', [TaskController::class, 'deleteTask']);
+Route::get('tasks/{id}', [TaskController::class, 'getTask']);
+Route::get('tasks', [TaskController::class, 'getTasks']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
