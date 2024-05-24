c281f95e0e7e0ba5ca5ed83e4a52b1f9403593a1
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2024-05-24 00:46:49 -0600

    se agregaron las rutas correctas

--- routes/userRoutes.php
+++ routes/userRoutes.php
@@ -8,13 +8,11 @@ use App\Http\Controllers\ProfileController;
     Route::post('register', 'UserController@register');
 
     //rutas del controlador ProfileController
-    Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
-    Route::get('/profiles/create', [ProfileController::class, 'create'])->name('profiles.create');
-    Route::post('/profiles', [ProfileController::class, 'store'])->name('profiles.store');
-    Route::get('/profiles/{profile}', [ProfileController::class, 'show'])->name('profiles.show');
-    Route::get('/profiles/{profile}/edit', [ProfileController::class, 'edit'])->name('profiles.edit');
-    Route::put('/profiles/{profile}', [ProfileController::class, 'update'])->name('profiles.update');
-    Route::delete('/profiles/{profile}', [ProfileController::class, 'destroy'])->name('profiles.destroy');
+    Route::post('/profiles', [ProfileController::class, 'addProfile']);
+    Route::put('/profiles/{id}', [ProfileController::class, 'updateProfile']);
+    Route::delete('/profiles/{id}', [ProfileController::class, 'deleteProfile']);
+    Route::get('/profiles/{id}', [ProfileController::class, 'getProfile']);
+    Route::get('/profiles', [ProfileController::class, 'getProfiles']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
