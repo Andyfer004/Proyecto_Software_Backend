8b4656ce189a9e75cf6039141b047e32bf5f3c16
ebarrera2019263 <ebarrera2019263>
2024-05-23 23:04:15 -0600

    se cargo controlador

--- app/Http/Controllers/prioritiesController.php
+++ app/Http/Controllers/prioritiesController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Priorities;
 use Illuminate\Http\Request;
 
-class prioritiesController extends Controller
+class PrioritiesController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        $priorities = Priorities::all();
+        return response()->json($priorities);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'namepriority' => 'required|string|max:255',
+        ]);
+
+        $priority = Priorities::create($request->all());
+        return response()->json($priority, 201);
+    }
+
+   
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'namepriority' => 'required|string|max:255',
+        ]);
+
+        $priority = Priorities::findOrFail($id);
+        $priority->update($request->all());
+        return response()->json($priority);
+    }
+
+    
+    public function destroy($id)
+    {
+        $priority = Priorities::findOrFail($id);
+        $priority->delete();
+        return response()->json(null, 204);
+    }
 }
