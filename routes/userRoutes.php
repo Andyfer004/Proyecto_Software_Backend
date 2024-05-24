fbbf3840e8d5a79223e8d762e023ec3bf3fedf8e
ebarrera2019263 <ebarrera2019263>
2024-05-24 11:29:32 -0600

    se agregaron la rutas

--- routes/userRoutes.php
+++ routes/userRoutes.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\SubtasksController;
 
 
     Route::post('login', 'UserController@login');
@@ -18,6 +19,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('users/{id}', 'UserController@update');
     Route::delete('users/{id}', 'UserController@deactivateAccount');
 
+});
+
+Route::prefix('subtasks')->group(function () {
+    Route::post('/', [SubtasksController::class, 'addSubtask']);
+    Route::put('/{id}', [SubtasksController::class, 'updateSubtask']);
+    Route::delete('/{id}', [SubtasksController::class, 'deleteSubtask']);
+    Route::get('/{id}', [SubtasksController::class, 'getSubtask']);
+    Route::get('/', [SubtasksController::class, 'getSubtasks']);
 });
 
 Route::get('/not_auth', function () {
