ac5a121afa50563a34727e3fea989196edc80c8d
Jorge162017 <lop221038@uvg.edu.gt>
2024-05-24 10:48:24 -0600

    Rutas de eventos

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StatusController;
+use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,18 @@ use App\Http\Controllers\StatusController;
 Route::post('status', [StatusController::class, 'addStatus']);
 Route::put('status/{id}', [StatusController::class, 'updateStatus']);
 Route::delete('status/{id}', [StatusController::class, 'deleteStatus']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+//Rutas de eventos
+Route::post('events', [EventController::class, 'addEvent']);
+Route::put('events/{id}', [EventController::class, 'updateEvent']);
+Route::delete('events/{id}', [EventController::class, 'deleteEvent']);
+Route::get('events/{id}', [EventController::class, 'getEvent']);
+Route::get('events', [EventController::class, 'getEvents']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
