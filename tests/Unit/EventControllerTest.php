fdb9f08d72ac37e1c1832a8d54f22def5c427cc7
DiederichSolis <sol22952@uvg.edu.gt>
2024-08-13 22:35:24 -0600

    feat: creacion de la prueba unitaria para Eventcontroller

---
+++ tests/Unit/EventControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class EventControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
