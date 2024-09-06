02414b640088ffb68fc1436537454375f5a7dba9
davisBold <142925428+davisBold@users.noreply.github.com>
2024-09-06 17:39:29 -0600

    changes routes

--- routes/userRoutes.php
+++ routes/userRoutes.php
@@ -26,13 +26,6 @@ Route::prefix('subtasks')->group(function () {
 });
 
 
-Route::prefix('reminders')->group(function () {
-    Route::post('/', [RemindersController::class, 'addReminder']);
-    Route::put('/{id}', [RemindersController::class, 'updateReminder']);
-    Route::delete('/{id}', [RemindersController::class, 'deleteReminder']);
-    Route::get('/{id}', [RemindersController::class, 'getReminder']);
-    Route::get('/', [RemindersController::class, 'getReminders']);
-});
 
 
 Route::get('/not_auth', function () {
